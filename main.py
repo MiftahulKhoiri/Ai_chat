3c951e9b3f1ac28f5032a6d81e5afdefde92af10
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-28 20:06:54 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from core.nlp_engine import NLPEngine
+
+engine = NLPEngine()
+
+# TRAIN SEKALI
+engine.train_language_model("data/training_data.xlsx")
+
+# PREDIKSI
+while True:
+    text = input("Kamu: ")
+    if text.lower() == "exit":
+        break
+
+    next_word = engine.predict_next_word(text)
+    if next_word:
+        print(f"Bot: mungkin kata berikutnya '{next_word}'")
+    else:
+        print("Bot: saya belum tahu kelanjutannya")
