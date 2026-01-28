d880bd3251b03c2ebffb9dac50f236f93f69aa68
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-28 22:02:20 +0800

    Update conversation_memory.py

--- core/conversation_memory.py
+++ core/conversation_memory.py
@@ -1,12 +1,14 @@
 class ConversationMemory:
     def __init__(self, max_turns=5):
         self.max_turns = max_turns
-        self.history = []
+        self.history = []  # (user, bot, sentiment_score)
 
-    def add(self, user_text, bot_text):
-        self.history.append((user_text, bot_text))
+    def add(self, user_text, bot_text, score):
+        self.history.append((user_text, bot_text, score))
         if len(self.history) > self.max_turns:
             self.history.pop(0)
 
-    def get_context(self):
-        return " ".join([u for u, _ in self.history])
+    def avg_sentiment(self):
+        if not self.history:
+            return 0
+        return sum(s for _, _, s in self.history) / len(self.history)
